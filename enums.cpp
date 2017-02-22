9211df8cc0cbd470d8ced524541d3b7098ccc43d
Diorgenes Morais <diorgenesmorais@gmail.com>
2017-02-21 21:42:05 -0300

    Enumeração - coleção de valores

---
+++ enums.cpp
@@ -0,0 +1,16 @@
+/**
+ * Constantes enumeradas (enum)
+ *
+ * @autor Diorgenes Morais
+ */
+#include <iostream>
+using namespace std;
+
+// podemos iniciar a coleção
+enum months_t {janeiro=1, fevereiro, marco, abril, maio, junho, julho, agosto, setembro, outubro, novembro, dezembro};
+
+int main(){
+  cout << "Março é o " << maio << "º mês do ano" << endl;
+
+  return 0;
+}
